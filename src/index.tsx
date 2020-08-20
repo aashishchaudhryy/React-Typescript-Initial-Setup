8582fc938b81366a42ebfa9663f1d22b8ee71dc0
Aashish Chaudhary <ashish.ch@conqsys.com>
2020-08-20 14:45:29 +0530

    changes

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import {hot} from 'react-hot-loader/root';
 import * as serviceWorker from './serviceWorker';
+
+const rootElement = document.getElementById('root');
 
 const app = (
   <HashRouter>
@@ -11,7 +14,7 @@ const app = (
   </HashRouter>
 )
 
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(app, rootElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
